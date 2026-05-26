b8b5e86b370da27071ed7c67d404f0940ec7e554
Nicole Wren <nicole@wren.systems>
2026-05-26 15:43:26 -0700

    feat: add aria-labels to controls

--- src/components/react/PgnViewer/Controls/index.tsx
+++ src/components/react/PgnViewer/Controls/index.tsx
@@ -13,21 +13,41 @@ export const Controls = (props: ControlActions) => {
 	return (
 		<div>
 			<div className="button-section">
-				<button title="Back" onClick={() => props.onBackButtonClick()}>
+				<button
+					title="Back"
+					aria-label="Back"
+					onClick={() => props.onBackButtonClick()}
+				>
 					<ArrowLeft />
 				</button>
-				<button title="Forward" onClick={() => props.onForwardButtonClick()}>
+				<button
+					title="Forward"
+					aria-label="Forward"
+					onClick={() => props.onForwardButtonClick()}
+				>
 					<ArrowRight />
 				</button>
-				<button title="Save" onClick={() => props.onSaveButtonClick()}>
+				<button
+					title="Save"
+					aria-label="Save"
+					onClick={() => props.onSaveButtonClick()}
+				>
 					<Save strokeWidth={'1px'} />
 				</button>
 			</div>
 			<div className="button-section">
-				<button title="Copy FEN" onClick={() => props.onCopyButtonClick()}>
+				<button
+					title="Copy FEN"
+					aria-label="Copy FEN"
+					onClick={() => props.onCopyButtonClick()}
+				>
 					<Copy strokeWidth={'1px'} />
 				</button>
-				<button title="Undo" onClick={() => props.onUndoButtonClick()}>
+				<button
+					title="Undo"
+					aria-label="Undo"
+					onClick={() => props.onUndoButtonClick()}
+				>
 					<Undo2 />
 				</button>
 			</div>
